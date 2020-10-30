0fcac699dc376a1fe72a2dca383eb67cffde2839
Andreas6400 <58548723+Andreas6400@users.noreply.github.com>
2020-10-30 12:11:19 +0100

    Update roemische Zahlen.py
    Final commit

--- roemische Zahlen.py
+++ roemische Zahlen.py
@@ -2,25 +2,31 @@
 Aufgabe: Schreiben Sie ein Python-Programm, das eine beliebige römische Zahl in eine
          „gewöhnliche” Dezimalzahl umrechnet.
 '''
-
-
-roemische_zahlen = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000} #Erstelle ein Dictionary
+# Erstelle ein Dictionary, "übersetze" römische Zahlen in Dezimalzahlen
+roemische_zahlen = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+# Instanziiere eine Variable mit dem Namen "dezimal_zahlen"
 dezimal_zahlen = 0
 
-
+# User Input wird in input_roemisch "gespeichert"
 if __name__ == "__main__":
-    input_roemisch = input("Römische Zahl eingeben: ")
+    input_roemisch = input("Römische Zahl eingeben: ") 
 
-
+# Instanziiere eine Variable mit dem Namen "vorherige_zahl"
 vorherige_zahl = 0
+
+# In Python ist es möglich, über einen String zu iterieren... Iteriere mit einer for-Schleife
 for char in input_roemisch:
-    if char in roemische_zahlen:
+        # Wenn die nachfolgende Ziffer/Zahl größer ist als die vorherige Ziffer/Zahl...
         if roemische_zahlen[char] > vorherige_zahl:
+            # Subtrahiere die vorherige Zahl von der nachfolgenden Zahl
             dezimal_zahlen -= vorherige_zahl
+        # Wenn die nachfolgende Ziffer/Zahl kleiner oder gleich groß ist wie die vorherige Ziffer/Zahl...
         else:
+            # Addiere die vorherige Zahl mit der nachfolgenden Zahl
             dezimal_zahlen += vorherige_zahl
-    vorherige_zahl = roemische_zahlen[char]
-dezimal_zahlen += vorherige_zahl
+            # Übersetze die vorherige römische Zahl in eine Dezimalzahl
+        vorherige_zahl = roemische_zahlen[char]
+        # Addiere die vorherige Zahl zur Dezimalzahl
+dezimal_zahlen += vorherige_zahl 
 
-
-print("Als Dezimalzahl: " + str(dezimal_zahlen))
+print("Als Dezimalzahl: " + str(dezimal_zahlen)) # Ausgabe der Dezimalzahl
